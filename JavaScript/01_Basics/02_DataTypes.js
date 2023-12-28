2c67b93da24fd9e0d6afadaf5e58f057829c34e6
Charlie-Chows <sujithkasthuri247@gmail.com>
2023-12-28 10:21:08 +0530

    datatypes, use strict, typeof

---
+++ JavaScript/01_Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let age = 7;
+let name = "sujith";
+let state;
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null);   // object
+console.log(typeof null === "object");  // true
+
+/**
+ * DATA TYPES
+ *  number => 2 to power 53
+ *  bigint
+ *  string => ""
+ *  boolean => true/false
+ *  null => standalone value
+ *  undefined 
+ *  symbol => uniqueness
+ *  object
+ *  array 
+ *  etc....
+ */
