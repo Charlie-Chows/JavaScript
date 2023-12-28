70f5f7d2dfaa9f734ea107c3d88508faf0ca9cc0
Charlie-Chows <sujithkasthuri247@gmail.com>
2023-12-28 11:25:19 +0530

    use strict, type of, data types

--- JavaScript/01_Basics/02_DataTypes.js
+++ JavaScript/01_Basics/02_DataTypes.js
@@ -1,12 +1,28 @@
 "use strict";
 
-let age = 7;
-let name = "sujith";
-let state;
+// With strict mode
+var x = 10;
 
-console.log(typeof undefined);  // undefined
-console.log(typeof null);   // object
-console.log(typeof null === "object");  // true
+function displayValue() {
+    y = 20; // Error in strict mode: Uncaught ReferenceError: y is not defined
+
+    console.log(x);
+    console.log(y);
+  }
+  
+  displayValue();
+  console.log(x);
+  console.log(y); // Error in strict mode: Uncaught ReferenceError: y is not defined
+
+  /**
+   * If i remove use strict the output will be 10 20 10 20
+   */
+
+// Unexpected behavior of js engine/compiler/interpreter
+  console.log(typeof undefined); // undefined
+  console.log(typeof null); // object
+  console.log(typeof null === "object"); // true
+
 
 /**
  * DATA TYPES
